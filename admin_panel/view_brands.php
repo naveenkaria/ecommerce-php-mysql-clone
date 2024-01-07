464b53042cd6040f590dc511160888a1efc3773f
Naveen Karia <naveen.karia29@gmail.com>
2024-01-07 22:46:08 +0500

    admin confirm before deleting items

--- admin_panel/view_brands.php
+++ admin_panel/view_brands.php
@@ -27,9 +27,28 @@
                 </td>
                 <td><a href="index.php?edit_brand=<?php echo $brand_id ?>" class='text-dark'><i
                             class="fa-solid fa-pen-to-square"></i></a></td>
-                <td><a href="index.php?delete_brand=<?php echo $brand_id ?>" class='text-dark'><i
-                            class="fa-solid fa-trash"></i></a></td>
+                <td><a href="index.php?delete_brand=<?php echo $brand_id ?>" type="button" class="btn btn-danger text-light"
+                        data-toggle="modal" data-target="#exampleModal"><i class="fa-solid fa-trash"></i></td>
             </tr>
         <?php } ?>
     </tbody>
 </table>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" role="dialog"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <h4>Are you sure you want to delete this?</h4>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><a
+                        href="./index.php?view_brands" class="text-light text-decoration-none">No</a></button>
+                <button type="button" class="btn btn-primary"><a href="index.php?delete_brand=<?php echo $brand_id ?>"
+                        class='text-light text-decoration-none'>Yes</a></button>
+            </div>
+        </div>
+    </div>
+</div>
